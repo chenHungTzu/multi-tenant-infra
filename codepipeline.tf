d643153b4ad24933688928c7993053ecb3adbdd0
anthonychen <motx2152000@gmail.com>
2024-03-10 16:23:02 +0800

    code-build

--- codepipeline.tf
+++ codepipeline.tf
@@ -149,7 +149,7 @@ resource "aws_codebuild_project" "project" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:latest"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
